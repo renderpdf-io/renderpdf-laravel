3fef576631797b2be45e4abbb920bf8c8c43f2eb
Seth Phat <phattranminh96@gmail.com>
2024-08-03 21:05:46 +0700

    RenderPDF.io Driver for Laravel

--- src/RenderPdfIo.php
+++ src/RenderPdfIo.php
@@ -7,6 +7,9 @@ use RenderPdfIo\Services\RenderPdfIoService;
 use RenderPdfIo\Services\RenderPdfOptions;
 
 /**
+ * Welcome to RenderPDF.io, convert HTML to modern PDF files in seconds
+ * Get your API Key at: https://renderpdf.io (we offer high usage for FREE plan - highest on the market)
+ *
  * @method static string render(RenderPdfOptions $options)
  * @method static string renderAsync(RenderPdfOptions $options)
  *
